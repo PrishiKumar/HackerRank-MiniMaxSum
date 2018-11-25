97bbf030389d50a87eed08a49f9c57618489cb4e
Prishi Kumar <prishi.prishi93@gmail.com>
2018-11-25 23:33:23 +0530

    Create Solution.scala

---
+++ Warm-Up-Challenges/Birthday Candles/Solution.scala
@@ -0,0 +1,35 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    def birthdayCakeCandles(ar: Array[Int]): Int = {
+    val max_map = ar.groupBy(identity).mapValues(_.size)
+    val max_value = max_map.max
+    val count = max_value._2
+    return count
+    }
+
+    def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val arCount = stdin.readLine.trim.toInt
+
+        val ar = stdin.readLine.split(" ").map(_.trim.toInt)
+        val result = birthdayCakeCandles(ar)
+
+        printWriter.println(result)
+
+        printWriter.close()
+    }
+}
